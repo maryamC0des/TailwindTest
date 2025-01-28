b31778c3854772fa9172e9cf8360a7bb35b7f69f
MaryamSafwat <maryamsafwt4@gmail.com>
2025-01-28 19:32:49 +0200

    tailwind

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+  setMode();
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+document.getElementById("toggle").addEventListener("change", function(){
+    if (this.checked){
+        localStorage.theme = "dark";
+    }else {
+        localStorage.theme = "light";
+    }
+   setMode();
+});
+
+function setMode(){
+    document.documentElement.classList.toggle(
+        'dark',
+        localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      )
+}
